e53d8ab945abb4df44ddb8fec29554667ed8cc09
viqueen <hasnae@viqueen.org>
2025-01-27 12:48:22 +1100

    fix up lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,95 @@
-const config = {
-    ignores: [
-        "dist/",
-        "node_modules/",
-        "coverage/",
-        ".idea/"
-    ],
-}
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default config;
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import labsetEslint from "@labset-eslint/eslint-plugin";
+// eslint-disable-next-line import/no-unresolved
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+// eslint-disable-next-line import/no-unresolved
+import tsParser from "@typescript-eslint/parser";
+import _import from "eslint-plugin-import";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["node_modules", "dist", "coverage", ".idea"],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:import/recommended",
+    ),
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+      "@labset-eslint": labsetEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          modules: true,
+        },
+      },
+    },
+
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".ts", ".tsx"],
+        },
+      },
+    },
+
+    rules: {
+      "@labset-eslint/license-notice": [
+        "error",
+        {
+          license: "Apache-2.0",
+          copyRightYear: "<update-me>",
+          copyRightName: "<update-me>",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "_",
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          pathGroupsExcludedImportTypes: ["builtin"],
+        },
+      ],
+    },
+  },
+];
